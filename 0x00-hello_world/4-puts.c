661c1be0c3127b2089f899f66f2fb11e794e5b80
hamza-25 <me677499@gmail.com>
2023-03-16 12:08:06 -0700

    4 nothing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
  * main - Entry point 
  *
- * Description: Puts function 
+ * Description: A C program that prints with put function 
  *
  * Return: Always 0 (Success)
 */
@@ -10,5 +11,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
