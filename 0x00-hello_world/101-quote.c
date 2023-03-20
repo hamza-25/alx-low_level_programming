c57fb8649ce2f7cb9827b89eba745574319f2fd0
hamza-25 <me677499@gmail.com>
2023-03-20 16:19:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+* main - Entry point
+* Description: A C program that prints with printf function
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+write(1, quo, 59);
+return (0);
+}
