0fad266ac5210aec1ef01495009c9f1b269620b0
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-03-21 16:55:37 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,15 @@
 */
 int main(void)
 {
-printf("_puchar\n");
+putchar("_puchar\n");
+putchar("_");
+putchar("p");
+putchar("u");
+putchar("t");
+putchar("c");
+putchar("h");
+putchar("a");
+putchar("r");
+putchar("\n");
 return (0);
 }
