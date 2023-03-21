604c6c965c7284ee55e0e678c0ecd1ad82e08a33
hamza-25 <me677499@gmail.com>
2023-03-21 07:13:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: A C program that prints with printf function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("_puchar\n");
+return (0);
+}
