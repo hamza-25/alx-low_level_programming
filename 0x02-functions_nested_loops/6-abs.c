0274457506b6748210c609eeedd1435be2abb0cc
hamza-25 <me677499@gmail.com>
2023-03-21 15:42:13 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * print_sign - function to check if character is lowercase
+ *
+ * _abs(): takes integer type input for function
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
+
+int _abs(int)
+{
+int a;
+a = abs(int);
+_putchar(a);
+_putchar('\n');
+}
