bcb1bbd5f0762240640e400e3960a0217cf9e0a4
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-03-22 09:45:36 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,30 @@
 
 void times_table(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-printf("%d,  ", j * i);
-}
-printf("\n");
-}
+ int i, j, mult;
+    for (i = 0; i <=9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            mult = j * i;
+            if(mult <= 9)
+            {
+                printf("%d", mult);
+                if(j != 9)
+                {
+                    printf(",  ");
+                }
+            }
+            else
+            {
+                 printf("%d", mult);
+                 if(j != 9)
+                {
+                    printf(", ");
+                }
+            }
+            
+        }
+      printf("\n");
+    }
 }
