02f282c3b3900d3ea2e29017fe21bcebf463d624
hamza-25 <me677499@gmail.com>
2023-03-22 10:46:16 -0700

    103-fibonacci.c v2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,15 +6,19 @@
 */
 int main(void)
 {
-float total_sum
+float total_sum;
 unsigned long fib1 = 0, fib2 = 1, sum;
 while (1)
 {
 sum = fib1 + fib2;
 if (sum > 4000000)
+{
 break;
+}
 if ((sum % 2) == 0)
+{
 total_sum += sum;
+}
 fib1 = fib2;
 fib2 = sum
 }
