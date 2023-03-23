e38cdaab3ac00f985e88f74294de3069b88e8342
hamza-25 <me677499@gmail.com>
2023-03-23 09:41:47 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_line - printing a line
+ * description: print line with n time
+ * @n: number of time to print underscore
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
