494bf2c2a671b6eed33eff92cc8b352f4bf70895
hamza-25 <me677499@gmail.com>
2023-03-23 15:05:57 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,6 +3,7 @@
 /**
  * print_triangle - printing a line diagonal
  * @size: number of diagonal
+ * Return: Always 0 (Success)
 */
 
 void print_triangle(int size)
@@ -10,10 +11,12 @@ void print_triangle(int size)
 	int hght, base;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-	for (hght = 1 ; hght <= size; hght++)
+		for (hght = 1 ; hght <= size; hght++)
 	{
 		for (base = 1; base <= size; base++)
 		{
