114ca652db333aef85ec84ca79883c718b4840a2
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-03-24 21:50:05 +0000

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,55 @@
 #include <stdio.h>
 #include <math.h>
 
+double _sqrt(double a) {
+    
+  double i = 0;
+  double j = a / 2;
+
+  while (j != i) {
+    i = j;
+    j = (a / i + i) / 2;
+  }
+  
+  return j;
+  
+}
+
 /**
- * main - print the largest prime factor
+ * largest_prime_factor - print the largest prime factor
+ * @num: number to find
+*/
+
+void largest_prime_factor(long int num)
+{
+        int prmNum, largest;
+        
+        while(num % 2 == 0)
+        {
+                num = num /2;
+        }
+        for (prmNum = 3; prmNum <= _sqrt(num); prmNum += 2)
+        {
+                while (num % prmNum == 0)
+                {
+                num = num / prmNum;
+                largest = prmNum;
+                }
+        }
+        if (num > 2)
+        {
+                largest = num
+        }
+        printf("%d\n", largest);
+}
+
+/**
+ * main - entry point
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        long x, maxf;
-        long number = 612852475143;
-        double square = sqrt(number);
-
-        for (x = 1; x <= square; x++)
-        {
-                if (number % x == 0)
-                {
-                        maxf = number / x;
-                }
-        }
-        printf("%d\n", maxf);
+        largest_prime_factor(612852475143);
         return (0);
 }
