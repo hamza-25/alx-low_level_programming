b6abe7cc19bbe1d3daf842d456ed574d795783e0
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-03-24 14:24:14 +0000

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,10 +8,10 @@ void print_number(int n)
         unsigned int num = n;
         if (n < 0)
         {
-                _putcar('-');
+                _putcar(45);
                 num = -num;
         }
-        if ((num / 10) > 0)
+        if (num / 10)
                 print_number(num / 10);
-        _putchar((num % 10) + 48);
+        _putchar(num % 10 + '0');
 }
