a62158aef87612f809aaa97280d15666452f7e73
hamza-25 <me677499@gmail.com>
2023-03-27 08:08:59 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that count lenght
+ * description: count lenght of string
+ * @s:string
+ * Return: always 0
+*/
+
+int _strlen(char *s)
+{
+	int i;
+	int strlen = 0;
+
+	for (i = 0; i < sizeof(*s) - 1; i++)
+	{
+	strlen += 1;
+	}
+}
