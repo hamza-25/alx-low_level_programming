755e9b018b49fdf80a4f546d6ffad7f9deddf3d1
hamza-25 <me677499@gmail.com>
2023-03-27 07:17:48 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reset_to_98:  reset number
+ * description: number 98 greather or less reset to it
+ * @n: pointer number to reset
+*/
+
+void reset_to_98(int *n)
+{
+	int diff;
+
+	if (*n >= 98)
+	{
+		diff = *n - 98;
+		*n = *n - diff;
+	}
+	else
+	{
+		diff = 98 - *n;
+		*n = *n + diff;
+	}
+	return (*n);
+}
