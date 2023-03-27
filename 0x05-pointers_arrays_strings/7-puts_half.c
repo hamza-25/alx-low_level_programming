100029b617700bb1e23e843d17b47073e71b2e48
hamza-25 <me677499@gmail.com>
2023-03-27 16:27:18 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - function that print
+ * description: string printing rev
+ * @str: string to print
+ * Return: always 0
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\n'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
