7eb60adaa3db001f1b43a0da22f8d73604b5a5a1
hamza-25 <me677499@gmail.com>
2023-03-27 08:51:27 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - function that print str rev
+ * description: string printing rev
+ * @s: string to print
+ * Return: always 0
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	_strlen(str);
+	for (i =  _strlen(s); i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
