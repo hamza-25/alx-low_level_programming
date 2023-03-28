4f8db3560ae21859ed67f39d2f377b008f636f4e
hamza-25 <me677499@gmail.com>
2023-03-28 10:46:42 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * description: rand passwd
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+	char c;
+	int sum;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 120;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
