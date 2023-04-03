675f6f92ffa3da25b24a134555b4bc7371e9dea5
hamza-25 <me677499@gmail.com>
2023-04-03 12:49:56 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - function that search for char
+ * @s: string for shearch
+ * @c: character to search
+ * Return: return s
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			printf("%c", *s);
+			return (s);
+		}
+		s++;
+	}
+	return (s);
+}
