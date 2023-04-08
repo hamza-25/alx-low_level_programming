1476c2108599d7b12e970d83c94ce8246ddbcec1
hamza-25 <me677499@gmail.com>
2023-04-08 06:38:21 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: count argc
+ * @argv: string of array from args
+ * descriptio: argc and argv function
+ * Return: Always return 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
