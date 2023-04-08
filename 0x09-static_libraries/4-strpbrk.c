573ed878de675052657c91d22bcaf57d4ad6973d
hamza-25 <me677499@gmail.com>
2023-04-08 04:25:24 -0700

    statis_libraries

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strpbrk - function that searches a string 
+* @s: first occurrence 
+* @accept: matches one of the bytes, or @NULL 
+* Return: byte
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int k;
+
+while (*s)
+{
+for (k = 0; accept[k]; k++)
+{
+if (*s == accept[k])
+return (s);
+}
+s++;
+}
+return ('\0');
+}
