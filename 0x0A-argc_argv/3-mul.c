24f3266e6655a738db3cb7b1a35d18a07eb52d19
hamza-25 <me677499@gmail.com>
2023-04-08 07:08:32 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ * @argc: count argc
+ * @argv: string of array from args
+ * descriptio: argc and argv function
+ * Return: Always return 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		printf("\n");
+		return (1);
+	}
+	else
+	{
+		int n1 = atoi(argv[1]);
+		int n2 = atoi(argv[2]);
+		int result;
+
+		result = n1 * n2;
+		printf("%d\n", result);
+	}
+	return (0);
+}
