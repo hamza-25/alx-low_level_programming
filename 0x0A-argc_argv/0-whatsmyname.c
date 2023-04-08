712dfe61e947bfdde169ef229a66ac3ed39727c1
hamza-25 <me677499@gmail.com>
2023-04-08 06:26:19 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * descriptio: argc and argv
+ * return (0)
+*/
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}	
