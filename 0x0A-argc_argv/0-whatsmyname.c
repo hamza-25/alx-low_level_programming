9fdd2fcaa7ef9cd39d2be015c16c35f19f084c26
hamza-25 <me677499@gmail.com>
2023-04-08 06:32:58 -0700

    0-whatsmyname.c v2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,13 @@
 
 /**
  * main - entry point
- * descriptio: argc and argv
- * return (0)
+ * @argc: count argc
+ * @argv: string of array from args
+ * descriptio: argc and argv function
+ * Return: Always return 0
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
-}	
+}
