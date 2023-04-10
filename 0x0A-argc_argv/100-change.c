744e5ee9056b35775a90221eade43e885142def5
hamza-25 <me677499@gmail.com>
2023-04-10 16:24:23 -0700

    100-change.c v3

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,14 +8,21 @@
  * main - entry point
  * @argc: count argc
  * @argv: string of array from args
- * descriptio: argc and argv function return cents
+ * description: argc and argv function return cents
  * Return: Always return 0
 */
 int main(int argc, char *argv[])
 {
+	if (argc == 1)
+	{
+		printf("Error\n");
+                return (1);
+	}
+	else
+	{
 	int i, back = 0, mny = atoi(argv[1]);
-	int cents[] = {25, 10, 5, 2, 1};
-
+	int coins[] = {25, 10, 5, 2, 1};
+	
 	if (mny < 0)
 	{
 		printf("%d\n", 0);
@@ -26,11 +33,11 @@ int main(int argc, char *argv[])
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (mny >= cents[i])
+			if (mny >= coins[i])
 			{
-				back += mny / cents[i];
-				mny = mny % cents[i];
-				if (mny % cents[i] == 0)
+				back += mny / coins[i];
+				mny = mny % coins[i];
+				if (mny % coins[i] == 0)
 				{
 					break;
 				}
@@ -38,10 +45,6 @@ int main(int argc, char *argv[])
 		}
 		printf("%d\n", back);
 	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 	}
 	return (0);
