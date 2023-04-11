4497066212fbe84bb0a6951fb5a3b9d99069aece
hamza-25 <me677499@gmail.com>
2023-04-11 13:13:17 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - create dimenional array
+ * description: dimrn array with width and height initailzed by 0
+ * @width: width of array
+ * @height: height of array
+ * Return: NULL value or pointer to the array initialized
+*/
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 && height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		int *ptr;
+		int i, j;
+
+		ptr = malloc((width * height) * sizeof(int));
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				ptr[i][j] = 0;
+			}
+		}
+		return (ptr);
+	}
+}
