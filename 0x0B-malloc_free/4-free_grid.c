7e053a0b8613c1ca15532ba1f1b4c1e51954190b
hamza-25 <me677499@gmail.com>
2023-04-11 15:22:49 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - create dimenional array
+ * description: dimrn array freed
+ * @grid: to free
+ * @height: height of array
+ * Return: NULL value or pointer to the array initialized
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
