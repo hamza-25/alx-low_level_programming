c8a4b30af6035822b0ce67d2f82dec9efb9d52e4
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-04-12 23:35:54 +0000

    delete_white_space

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,40 +13,37 @@
 char *argstostr(int ac, char **av)
 {
 int i, j, k = 0,len = 0;
-  char *ptr;
-  
-  if(ac == 0 || av == NULL)
-  {
-    return (NULL);
-  }
-    
-  for (i = 0; i < ac; i++)
-  {
-    for (j = 0; av[i][j] != '\0'; j++)
-  {
-    len++;
-  }
-  len++;
-  }
-  len++;
-  
-  ptr = (char*)malloc(sizeof(char) * len);
-  if (ptr == NULL)
-  {
-    return (NULL);
-  }
-  else
-  {
-    for (i = 0; i < ac; i++)
-  {
-    for (j = 0; av[i][j] != '\0'; j++, k++)
-  {
-      ptr[k] = av[i][j];
-  }
-  ptr[k] = '\n';
-  k++;
-  }
-  ptr[k] = '\0';
-  return (ptr);
-  }	
+char *ptr; 
+if(ac == 0 || av == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+len++;
+}
+len++;
+}
+len++;
+ptr = (char*)malloc(sizeof(char) * len);
+if (ptr == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++, k++)
+{
+ptr[k] = av[i][j];
+}
+ptr[k] = '\n';
+k++;
+}
+ptr[k] = '\0';
+return (ptr);
+}	
 }
