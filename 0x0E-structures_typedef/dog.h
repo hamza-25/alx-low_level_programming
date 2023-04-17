a7797aefbbac09c89c9652d466e8258f5904e761
hamza-25 <me677499@gmail.com>
2023-04-17 09:07:52 -0700

    dog.h v1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * description: make an object of strct dog
+*/
+
 struct dog
 {
 
