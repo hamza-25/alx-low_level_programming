3c2ff357db14eae51aff2033152713c79a5b37e5
hamza-25 <me677499@gmail.com>
2023-04-19 09:58:15 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - func array that's accept args
+ * @array: array
+ * @cmp: func to compair
+ * @size: lenght
+ * Return: return (1) if found
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && size && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		int *len = array + size - 1;
+		int i = 0;
+
+		while (array <= len)
+		{
+			if (cmp(*array) == 1)
+				return (i);
+			i++;
+			array++;
+		}
+	}
+}
