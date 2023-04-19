f6c4ce31a9220243f51af8731b84be33bdeb891f
hamza-25 <me677499@gmail.com>
2023-04-19 07:57:58 -0700

    0-print_name.c v2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,15 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
  * print_name - func that's accept two arg
  * @name: name to be printed
  * @f: func pointers
- * return: nothing
+ * return: void
 */
 
-void print_name_as_is(char *name)
-{
-    printf("Hello, my name is %s\n", name);
-}
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+
+	if (name && f)
+		f(name);
 }
