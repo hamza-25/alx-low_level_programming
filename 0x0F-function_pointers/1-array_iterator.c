029c149439520d49931aef857a8946b0cf955f2d
hamza-25 <me677499@gmail.com>
2023-04-19 09:17:55 -0700

    1-array_iterator.c v2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array && size && action)
 	{
-	int i;
-
-	for (i = 0; i < size; i++)
-	{
-	action(array[i]);
-	}
+		int* len = array + size - 1;
+		
+		while (array <= len)
+			action(*array++);
 	}
 }
