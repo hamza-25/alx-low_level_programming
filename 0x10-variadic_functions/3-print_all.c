b31f64a3e0fa4cfde655bbb0279fcda92f05c12b
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-04-20 18:35:12 +0000

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -24,8 +24,27 @@ void print_all(const char * const format, ...)
 			break;
 		} j++;
 		}
-		#include "switch.c"
-		i++;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ptr, int)), l = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ptr, int)), l = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double)), l = 1;
+				break;
+			case 's':
+				r = va_arg(ptr, char *), l = 1;
+				if (r)
+				{
+					printf("%s", r);
+					break;
+				}
+				printf("(nil)");
+				break;
+		} i++;
 	}
 	printf("\n"), va_end(ptr);
 }
