517c9f18c4b50fd331ad36bb5607d32c885c3322
hamza-25 <me677499@gmail.com>
2023-04-21 07:01:16 -0700

    3-print_all.c v5

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,50 +2,48 @@
 #include <stdio.h>
 /**
  * print_all - print all inputs
- * description: user choose the format printing
  * @format: forma to be printed
- * Return: void
 */
-	void print_all(const char * const format, ...)
-	{
-		va_list ptr;
-		unsigned int i = 0, j, l = 0;
-		char *r;
-		const char r_format[] = "csif";
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	unsigned int i = 0, j, l = 0;
+	char *r;
+	const char r_format[] = "csif";
 
-		va_start(ptr, format);
-		while (format && format[i])
+	va_start(ptr, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (r_format[0])
 		{
-			j = 0;
-			while (r_format[0])
-			{
-			if (format[i] == r_format[j] && l)
-			{
-				printf(", ");
-				break;
-			} j++;
-			}
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(ptr, int)), l = 1;
-					break;
-				case 'i':
-					printf("%d", va_arg(ptr, int)), l = 1;
-					break;
-				case 'f':
-					printf("%f", va_arg(ptr, double)), l = 1;
-					break;
-				case 's':
-					r = va_arg(ptr, char *), l = 1;
-					if (r)
-					{
-						printf("%s", r);
-						break;
-					}
-					printf("(nil)");
-					break;
-			} i++;
+		if (format[i] == r_format[j] && l)
+		{
+			printf(", ");
+			break;
+		} j++;
 		}
-		printf("\n"), va_end(ptr);
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ptr, int)), l = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ptr, int)), l = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double)), l = 1;
+				break;
+			case 's':
+				r = va_arg(ptr, char *), l = 1;
+				if (r)
+				{
+					printf("%s", r);
+					break;
+				}
+				printf("(nil)");
+				break;
+		} i++;
 	}
+	printf("\n"), va_end(ptr);
+}
