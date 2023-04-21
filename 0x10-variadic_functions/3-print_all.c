8673ab9c54783c214867462fe964ecb20d6ccc0b
hamza-25 <me677499@gmail.com>
2023-04-21 06:54:40 -0700

    3-print_all.c c5

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,85 @@
 #include "variadic_functions.h"
-	#include <stdio.h>
-	/**
-	 * print_all - print all inputs
-	 * @format: forma to be printed
-	 * Return: void
-	*/
-	void print_all(const char * const format, ...)
-	{
-		va_list ptr;
-		unsigned int i = 0, j, l = 0;
-		char *r;
-		const char r_format[] = "csif";
+#include <stdio.h>
+/**
+ * format_char - type of format
+ * @separator: separator to be printed
+ * @ap: array of args
+ * Return: void
+*/
+void format_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_list(ap, int));
+}
+/**
+ * format_int - type of format
+ * @separator: separator to be printed
+ * @ap: array of args
+ * Return: void
+*/
+void format_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_list(ap, int));
+}
+/**
+ * format_float - type of format
+ * @separator: separator to be printed
+ * @ap: array of args
+ * Return: void
+*/
+void format_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_list(ap, double));
+}
+/**
+ * format_string - type of format
+ * @separator: separator to be printed
+ * @ap: array of args
+ * Return: void
+*/
+void format_string(char *separator, va_list ap)
+{
+	char *str = va_list(ap, char *);
 
-		va_start(ptr, format);
-		while (format && format[i])
-		{
-			j = 0;
-			while (r_format[0])
-			{
-			if (format[i] == r_format[j] && l)
-			{
-				printf(", ");
-				break;
-			} j++;
-			}
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(ptr, int)), l = 1;
-					break;
-				case 'i':
-					printf("%d", va_arg(ptr, int)), l = 1;
-					break;
-				case 'f':
-					printf("%f", va_arg(ptr, double)), l = 1;
-					break;
-				case 's':
-					r = va_arg(ptr, char *), l = 1;
-					if (r)
-					{
-						printf("%s", r);
-						break;
-					}
-					printf("(nil)");
-					break;
-			} i++;
-		}
-		printf("\n"), va_end(ptr);
+	switch ((int)(!str))
+	{
+		case 1:
+			str = "(nil)";
 	}
+	printf("%s%s", separator, str);
+}
+/**
+ * print_all - print all inputs
+ * @format: forma to be printed
+ * Return: void
+*/
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *separator = "";
+	va_list ap;
+	alias_t aliases[] = {
+		{"i", format_int},
+		{"c", format_char},
+		{"s", formart_string},
+		{"f", format_float},
+		{NULL, NULL}
+	};
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (aliases[j].alias)
+		{
+			if (format[i] == aliases[j].alias[0])
+			{
+				aliases[j].fun(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
