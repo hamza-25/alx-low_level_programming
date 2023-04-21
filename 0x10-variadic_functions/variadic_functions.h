07bcc052c5ff31769a88f53ff81c15205f592a31
hamza-25 <me677499@gmail.com>
2023-04-21 06:58:32 -0700

    3-print_all.c v4

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,17 +3,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct alias - Struct alias
- * @alias: format 
- * @fun: function
-*/
-typedef struct alias
-{
-	char *alias;
-	void (*fun)(char *, va_list);
-} alias_t;
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
