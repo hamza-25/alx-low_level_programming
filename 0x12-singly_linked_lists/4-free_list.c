a5257b1da7c91fed232c86dc75fdc04980e20fed
hamza-25 <me677499@gmail.com>
2023-04-27 18:50:25 +0000

    4-free_list.c v3

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,16 @@
 void free_list(list_t *head)
 {
 	list_t *new;
-
+	
+	if (head)
+	{
 	new = head;
 	while (new)
 	{
 		new = new->next;
 		free(head->str);
+		free(head);
 		head = new;
+	}
 	}
 }
