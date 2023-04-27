775186c4ff0f2f93ead8ad8e69114b95d6563de8
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-04-27 13:43:45 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,11 +5,11 @@
  * print_list - function that print content of nodes
  * @h: first node
  * Return: lenght of nodes i
- *
 */
+
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	unsigned int i = 0;
 
 	while (h != NULL)
 	{
