e9e4c0c36fe3e3fa6ff260c9b52e32bdeaf465bc
hamza-25 <me677499@gmail.com>
2023-04-27 14:31:40 +0000

    v7

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,7 +17,9 @@ typedef struct list_s
 } list_t;
 
 #include <stdlib.h>
-int string_len(char *s);
+#include <stdio.h>
+#include <string.h>
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
