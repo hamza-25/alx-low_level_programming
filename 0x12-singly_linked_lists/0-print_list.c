142305e2c7f5c486656e936b4fff74a21571d2aa
hamza-25 <me677499@gmail.com>
2023-04-27 14:10:17 +0000

    add str_len func

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,22 @@
 #include "lists.h"
 
 /**
+ * string_len - to count str
+ * @s: str to be counted
+ * Return: len of str
+*/
+
+int string_len(char *s)
+{
+	int i = 0;
+	if (!s)
+		return (0);
+	while (s++)
+		i++;
+	return (i);
+}
+
+/**
  * print_list - function that print content of nodes
  * @h: first node
  * Return: lenght of nodes i
@@ -15,7 +31,7 @@ size_t print_list(const list_t *h)
 	{
 		i += 1;
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] (nil)\n", string_len(h->str));
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
