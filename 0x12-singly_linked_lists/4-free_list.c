c18ac746c8cb63a04bcf58c8580b77cc88604389
hamza-25 <me677499@gmail.com>
2023-04-27 18:37:55 +0000

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - function that add node in last of list
+ * @head: first node
+ * Return: void
+*/
+
+void free_list(list_t *head)
+{
+	list_t *new;
+	
+	new = malloc(sizeof(list_t));
+	new = head;
+	while (new)
+	{
+		new = new->next;
+		free(head);
+		head = new;
+	}
+
+	return (head);
+}
