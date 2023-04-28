06f5e67f53667604aba6c0fbb1430989872bb7e3
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-04-28 00:12:15 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,8 +13,8 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		if (!h->str)
+			printf("[%d] (nil)\n", 0);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
