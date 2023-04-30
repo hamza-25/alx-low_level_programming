7c9f47c206dab5d83a5608bebee6ff16960fd12b
hamza-25 <me677499@gmail.com>
2023-04-30 02:59:17 +0000

    9-insert_nodeint.c fix indexing higher than list v6

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!current || !new)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = current;
+		*head = new;
+		return (new);
+	}
 	while (current)
 	{
 		current = current->next;
