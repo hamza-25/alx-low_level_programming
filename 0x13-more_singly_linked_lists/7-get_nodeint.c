52dd4f3340e004f6e5d9d7a92fed7efbda317231
hamza-25 <me677499@gmail.com>
2023-04-30 00:22:38 +0000

    7-get_nodeint.c fixing the return index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *act = head;
 	unsigned int count = 0;
 
-	for (; act; index++)
+	for (; act; count++)
 	{
 		if (count == index)
 		{
