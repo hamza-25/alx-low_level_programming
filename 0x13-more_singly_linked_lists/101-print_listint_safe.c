7d8e2bf538b90e4c4ac3ca8349fb992340ac1391
hamza-25 <me677499@gmail.com>
2023-05-01 13:30:15 +0000

    101-print_listint_safe.c adding file

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint_safe - func to print list safe
+ * @h: the head of the list
+ * Return:  number of elements nodes
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num_of_elem = 0;
+
+	if (head)
+	{
+		for (; head != NULL; num_of_elem++)
+		{
+			printf("%p %d\n", (void *)&(head->n), head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		exit (98);
+	}
+	return (num_of_elem);
+}
