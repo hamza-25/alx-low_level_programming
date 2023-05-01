9084141da532133b90f62f692e918072f0b68d54
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-05-01 23:05:23 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
-	if (idx >= count || idx == 0)
+	if (idx >= count)
 	{
 		free(new);
 		return (NULL);
