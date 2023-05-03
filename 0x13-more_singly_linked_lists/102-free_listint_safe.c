b5736314ebdfdfe95f8c6a4515a6befd0f188ea4
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-05-03 23:46:38 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,18 +7,17 @@
 */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t num_of_elem = 0, size = 0;
+	size_t num_of_elem = 0;
 	listint_t *current = *h;
 
 	if (*h)
 	{
 	for (; current; num_of_elem++)
 	{
-		size += sizeof(current);
 		current = current->next;
 		free(*h);
 		*h = current;
 	}
 	}
-	return (size);
+	return (num_of_elem);
 }
