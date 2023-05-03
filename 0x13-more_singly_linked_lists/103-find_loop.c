9ca26f38cd9193f647f07d3bd5d77aeb697d4f31
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-05-03 21:12:31 +0100

    103 detect loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * detect_loop - func to detect loop
+ * @head: the head of the list
+ * Return:  if loop or not
+*/
+listint_t *detect_loop(listint_t *head)
+{
+    listint_t *slow = head;
+    listint_t *fast = head;
+
+    slow = slow->next;
+    fast = fast->next->next;
+    while (fast && fast->next)
+    {
+        if (slow == fast)
+            break;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (slow != fast)
+        slow = NULL;
+    return (slow);
+    slow = head;
+    while (slow != fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return (slow);
+}
+/**
+ * find_listint_loop - func to find start loop
+ * @head: the head of the list
+ * Return:  pointer start loop
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+    listint_t *p = head;
+
+    p = detect_loop(head);
+    return (p);
+}
