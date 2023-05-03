3db4568455018c5ddd04981828c056048cb2f245
hamza-25 <me677499@gmail.com>
2023-05-03 18:03:04 +0000

    102-free_listint_safe.c add file

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_listint_safe - func to list list safe
+ * @h: the head of the list
+ * Return:  number of elements nodes
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	size_t num_of_elem = 0;
+	listint_t *current = (*h);
+
+	if (h)
+	{
+	for (; (*h); num_of_elem++)
+	{
+		current = current->next;
+		free(*h);
+		(*h) = current;
+	}
+	}
+	return (num_of_elem);
+}
