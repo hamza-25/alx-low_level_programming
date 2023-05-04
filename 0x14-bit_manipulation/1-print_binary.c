1964e05f036e7058f9c365470eafa1b03d76705e
hamza-25 <me677499@gmail.com>
2023-05-04 23:11:16 +0000

    1-print_binary.c add file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary without % and /
+ * @n: number to be converted to binary
+*/
+void print_binary(unsigned long int n)
+{
+	int tap = 0;
+	int size = 8 * sizeof(n);
+
+	for (;size;)
+	{
+		size--;
+		if (1L << size && n)
+		{
+			_putchar('1');
+			tap ++;
+		}
+		else if (tap)
+		{
+			_putchar('0');
+		}
+	}
+	if (!tap)
+	{
+		_putchar('0');
+	}
+}
