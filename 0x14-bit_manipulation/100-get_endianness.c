cc757bf263877506d54f915ed1b46b46249bc499
hamza-25 <me677499@gmail.com>
2023-05-05 17:24:08 +0000

    100-get_endianness.c add files

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endiannes - function checks endian 
+ * Return: 1 if little endian or 0 if big endian
+*/
+int get_endianness(void)
+{
+	char* letter;
+	unsigned int i;
+
+	i = 1;
+	letter = i & (char *);
+	return (letter);
+}
