54cda42988689d422d88b8a6f88c503843a5bb92
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-05-05 01:02:51 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,24 +6,5 @@
 */
 void print_binary(unsigned long int n)
 {
-	int tap = 0;
-	int size = 8 * sizeof(n);
-
-	for (; size;)
-	{
-		size--;
-		if (1L << size && n)
-		{
-			_putchar('1');
-			tap++;
-		}
-		else if (tap)
-		{
-			_putchar('0');
-		}
-	}
-	if (!tap)
-	{
-		_putchar('0');
-	}
+	
 }
