af5bb004b6674cb7734af4126701a2a63fe7471a
hamza-25 <me677499@gmail.com>
2023-05-05 17:07:59 +0000

    add file5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits from index given
+ * @n: number to be converted to binary
+ * @index: index for exist bits
+ * Retur: count flip
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int counter = 0;
+	int j;
+	unsigned long int Xor = m ^ n;
+	unsigned long int act;
+	
+	i = 64;
+	while (j > 0)
+	{
+		act = Xor >> j;
+		if (act & 1)
+		{
+			counter++;
+		}
+		j--;
+	}
+	return (counter);
+}
