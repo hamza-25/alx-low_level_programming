68e196163ca157fbd6b5972616c1a30858cf526d
hamza-25 <me677499@gmail.com>
2023-05-05 16:09:58 +0000

    1-print_binary.c add increment

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,14 +9,13 @@ void print_binary(unsigned long int n)
 	int tap = 0;
 	int size = 8 * sizeof(n);
 
-	size--;
-	for (; size; size--)
+	for (; size;)
 	{
 		if (tap)
 		{
 			_putchar('0');
 		}
-		else if (size & 1L << size)
+		else if (size & 1L << --size)
 		{
 			_putchar('1');
 			tap++;
