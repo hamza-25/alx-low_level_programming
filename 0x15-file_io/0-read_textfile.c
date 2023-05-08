b210e514dad00efe28dbdd4a522f6b0c7680a5b2
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-05-09 00:53:36 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,8 +23,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 		return (0);
-	
-	wr = fwrite(buffer, sizeof(char), letters, fread(buffer, sizeof(char), letters, fp));
+	fread(buffer, sizeof(char), letters, fp)
+	wr = fwrite(buffer, sizeof(char), letters, fp);
 	fclose(fp);
 	free(buffer);
 	return (wr);
