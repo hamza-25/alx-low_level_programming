72415bdfb64696641466aed56dccc2a656d6029d
hamza-25 <me677499@gmail.com>
2023-05-09 17:47:01 +0000

    1-create_file.c v3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (ln)
 	{
 		wr = fwrite(text_content, sizeof(text_content), ln, fp);
-		if (wr == -1)
+		if (wr != ln)
 			return (-1);
 	}
 	fclose(fp);
