f178000799eda1e1ffe65d922855464d29f06cc1
hamza-25 <me677499@gmail.com>
2023-05-09 17:44:26 +0000

    1-create_file.c v2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "main.h"
 
+
 /**
  * create_file -  create a file and add content
  * @filename: name of the file
@@ -11,16 +12,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	FILE *fp;
-	int wr;
+	ssize_t wr = 0;
+	int ln;
 
+	ln = strlen(text_content);
 	if (filename == NULL)
 		return (-1);
 	fp = fopen(filename, "w");
 	if (!fp)
 		return (-1);
-	if (text_content)
+	if (ln)
 	{
-		wr = fwrite(text_content, sizeof(text_content), strlen(text_content), fp);
+		wr = fwrite(text_content, sizeof(text_content), ln, fp);
 		if (wr == -1)
 			return (-1);
 	}
