39ed062799be66596debf196e068cd7ec382a99d
hamza-25 <me677499@gmail.com>
2023-05-09 18:18:08 +0000

    1-create_file.c v5

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,13 @@ int create_file(const char *filename, char *text_content)
 	ssize_t wr = 0;
 	ssize_t ln;
 	
-	for (ln = 0; text_content[ln]; ln++)
-		;
-	ln++;
+	for (ln = 1; *text_content; )
+	{
+		text_content++;
+		ln++;
+	}
+	if (ln)
+		ln--;
 	if (filename == NULL)
 		return (-1);
 	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
