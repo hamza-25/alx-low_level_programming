e0545643b7f84b320375dd5fbd69d9968e8cbede
hamza-25 <me677499@gmail.com>
2023-05-09 19:18:51 +0000

    1-create_file.c return to head 12

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,11 +27,9 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
-	if (ln)
-	{
-		wr = write(fp, text_content, ln);
-		if (wr == -1)
-			return (-1);
+	wr = write(fp, text_content, ln);
+	if (wr == -1)
+		return (-1);
 	}
 	close(fp);
 	return (1);
