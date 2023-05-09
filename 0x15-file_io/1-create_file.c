1bb42a9e2bc9e922e7e3be33ec4ec0021545855e
hamza-25 <me677499@gmail.com>
2023-05-09 19:27:02 +0000

    final

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fp;
+	FILE *fp;
 	int wr = 0;
 	int ln = 0;
 
@@ -24,12 +24,12 @@ int create_file(const char *filename, char *text_content)
 		text_content++;
 		ln++;
 	}
-	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fp == -1)
+	fp = fopen(filename, "w");
+	if (!fp)
 		return (-1);
-	wr = write(fp, text_content, ln);
+	wr = fwrite(text_content, sizeof(char), ln, fp);
 	if (wr == -1)
 		return (-1);
-	close(fp);
+	fclose(fp);
 	return (1);
 }
