172f7be2c8fbe9172895b3027eada69331e168fa
hamza-25 <me677499@gmail.com>
2023-05-09 18:21:29 +0000

    1-create_file.c v6

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,15 +22,15 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (ln)
 		ln--;
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fp == -1)
 		return (-1);
 	if (ln)
 	{
 		wr = write(fp, text_content, ln);
-		if (wr != ln)
+		if (wr == -1)
 		{
 			close(fp);
 			return (-1);
