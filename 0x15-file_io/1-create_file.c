1d8e42c46ddfe68d27cc91c7089e1fd2c5391ae9
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-05-09 20:37:12 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,12 +33,6 @@ int create_file(const char *filename, char *text_content)
 		if (wr == -1)
 			return (-1);
 	}
-	else
-	{
-		wr = fwrite(text_content, sizeof(char), ln, fp);
-		if (wr == -1)
-			return (-1);
-	}
 	fclose(fp);
 	return (1);
 }
