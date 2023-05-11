5dec7afd38584aabca6584ec0ce98c69f05033ff
hamza-25 <me677499@gmail.com>
2023-05-11 23:19:54 +0000

    1-create_file.c fixing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,26 +13,26 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fp;
+	int fp;
 	int wr = 0;
 	int ln = 0;
 
 	if (!filename)
 		return (-1);
+	if (text_content)
+	{
 	while (*text_content)
 	{
 		text_content++;
 		ln++;
 	}
-	fp = fopen(filename, "w+");
-	if (!fp)
-		return (-1);
-	if (text_content)
-	{
-		wr = fwrite(text_content, sizeof(char), ln, fp);
-		if (wr == -1)
-			return (-1);
 	}
-	fclose(fp);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1);
+	wr = write(fp, text_content, ln);
+	if (wr == -1)
+		return (-1);
+	close(fp);
 	return (1);
 }
