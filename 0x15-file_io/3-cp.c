5b514b09c5ee22d9f63b44a23aa6b747aa009d13
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-05-12 01:06:35 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,14 +19,14 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	to = open(argv[2], O_WRONLY, O_CREAT | O_TRUNC, 0664);
-	if (to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
 		dprintf(STDERR_FILENO, ERR_READ, argv[1]), exit(98);
 	}
+	to = open(argv[2], O_WRONLY, O_CREAT | O_TRUNC, 0664);
+	if (to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	for (; (bytes = read(from, buffer, 1024)) > 0;)
 		if (write(to, buffer, bytes) != bytes)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
