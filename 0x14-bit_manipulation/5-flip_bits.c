c5a82f9a572fa2d1805855b0ab21af94f5247523
hamza-25 <me677499@gmail.com>
2023-07-06 14:26:49 +0000

    add func flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits from index given
+ * @n: number to be converted to binary
+ * @m: index for exist bits
+ * Return: count flip
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int counter = 0;
+	int j;
+	unsigned long int Xor = m ^ n;
+	unsigned long int act;
+
+	j = 64;
+	while (j > 0)
+	{
+		act = Xor >> j;
+		if (act & 1)
+			counter++;
+		j--;
+	}
+	return (counter);
+}
