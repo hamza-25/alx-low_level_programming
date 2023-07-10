1b3f0959765b53cb30c7983eef7e3992a26c3cd3
hamza-25 <me677499@gmail.com>
2023-07-10 20:36:55 +0000

    add return type

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	f_d = open(argv[1], O_RDONLY);
 	if (f_d == -1)
 		dprintf(STDERR_FILENO, "file can't open: %s\n", argv[1]), exit(98);
-	bytes = read(f_d, &l, sizeof(l))
+	bytes = read(f_d, &l, sizeof(l));
 	if (bytes < 1 || bytes != sizeof(l))
 		dprintf(STDERR_FILENO, "can't read: %s\n", argv[1]), exit(98);
 	if (l.e_ident[1] == 'E' && l.e_ident[0] == 0x7f && l.e_ident[2] == 'L' &&
@@ -32,4 +32,5 @@ int main(int argc, char **argv)
 	}
 	else
 		dprintf(STDERR_FILENO, "Error elf file: %s\n", argv[1]), exit(98);
+	return (0);
 }
