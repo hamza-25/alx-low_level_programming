7653e24169e5458f3096a4c28a5b91b2f537abec
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-07-10 23:47:19 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,20 +19,24 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-		dprintf(2, ERR_READ, argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, ERR_READ, argv[1]), exit(98);
+	}
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	for (; (bytes = read(from, buffer, 1024)) > 0;)
 	{
 		if (write(to, buffer, bytes) != bytes)
-			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (bytes == -1)
-		dprintf(2, ERR_READ, argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, ERR_READ, argv[1]), exit(98);
+	}
 	from = close(from);
 	to = close(to);
 	if (to < 0 || from < 0)
