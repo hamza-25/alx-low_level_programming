013a60d74bd7e3fb997760510ac96e5476f998dc
hamza-25 <me677499@gmail.com>
2023-07-10 13:42:21 +0000

    creat a file with a permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,27 +12,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fp;
+	int fp;
 	int wr = 0;
 	int ln = 0;
 
 	if (!filename)
 		return (-1);
 	if (text_content)
-	{
-		while (*text_content)
-		{
-			text_content++;
+		while (text_content[ln])
 			ln++;
-		}
-	}
-	fp = fopen(filename, "w+");
-	if (!fp)
+	fp = open(filename, O_RDWR | O_CREAT , 0600);
+	if (fp < 0)
 		return (-1);
-	wr = fwrite(text_content, sizeof(char), ln, fp);
+	wr = write(fp, text_content, ln);
 	if (wr == -1)
 		return (-1);
-	fclose(fp);
+	close(fp);
 	return (1);
 }
 
