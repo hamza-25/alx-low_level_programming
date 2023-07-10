13ae75bb38b5fa8a3e0be0b5c61dbd52ac43e1a4
hamza-25 <me677499@gmail.com>
2023-07-10 13:53:21 +0000

    creat a file with a permission change in fd v2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 		while (text_content[ln])
 			ln++;
-	fp = open(filename, O_WRONLY | O_EXCL | O_CREAT , 0600);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fp < 0)
 		return (-1);
 	wr = write(fp, text_content, ln);
