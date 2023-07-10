1855347ed0227a17646245162d3d3db4a130c0a2
hamza-25 <me677499@gmail.com>
2023-07-10 12:41:49 +0000

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_file -  create a file and add content
+ * @filename: name of the file
+ * @text_content: content to be add into the file
+ * Return: return 1 on succees or -1 if fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	int wr = 0;
+	int ln = 0;
+
+	if (!filename)
+		return (-1);
+	if (text_content)
+	{
+		while (*text_content)
+		{
+			text_content++;
+			ln++;
+		}
+	}
+	fp = fopen(filename, "w+");
+	if (!fp)
+		return (-1);
+	wr = fwrite(text_content, sizeof(char), ln, fp);
+	if (wr == -1)
+		return (-1);
+	fclose(fp);
+	return (1);
+}
+
