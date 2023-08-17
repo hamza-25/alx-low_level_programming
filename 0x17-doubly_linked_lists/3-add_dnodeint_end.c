69b8f892229debe9f6684a7b1340275f1df95afb
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-17 12:45:04 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,27 +5,26 @@
  * @n: data to add
  * Return: the address of new element
 */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-        dlistint_t *new = NULL, *current = *head;
+	dlistint_t *new = NULL, *current = *head;
 
-        if (head == NULL)
-                return (NULL);
-        new = malloc(sizeof(dlistint_t));
-        if (!new)
-                return (NULL);
-        new->n = n;
-        new->next = NULL;
-        new->prev = NULL;
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-        while (current->next)
-                current = current->next;
-        current->next = new;
-        new->prev = current;
-        return (new);
+	while (current->next)
+		current = current->next;
+	current->next = new;
+	new->prev = current;
+	return (new);
 }
