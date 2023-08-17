d58685544a4cf4dac0d215e127f190235b7e6a10
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-17 15:22:08 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * dlistint_len - get length of dlist
+ * @h: head of dlist
+ * Return: number of node
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+	const dlistint_t *current = h;
+
+	while (current)
+	{
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
  * insert_dnodeint_at_index - add node with a given index
  * @h: head of dlist
  * @n: data to insert
