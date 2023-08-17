5b13630ebc2d2334727e98d4c27f0ac7e6447bef
hamza-25 <me677499@gmail.com>
2023-08-17 12:00:51 +0000

    free a double linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free double linked list
+ * @head: head of dlist
+ * Return: Void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
