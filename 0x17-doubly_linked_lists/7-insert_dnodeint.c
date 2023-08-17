fac2e30b19f500b8fe39dfaf694152204da881bf
hamza-25 <me677499@gmail.com>
2023-08-17 14:39:45 +0000

    add node ate certain index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,26 +30,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = *h, *new = NULL;
 	unsigned int len, count = 0;
 
-	if (!h || !(*h))
+	if (!h)
 		return (NULL);
 
 	len = dlistint_len((const dlistint_t *)(*h));
 	if (idx > len)
 		return (NULL);
 
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
 	if (idx == 0)
 	{
-		new = add_dnodeint(h, n);
+		new->prev = NULL;
+		new->next = *h;
+		if (*h)
+			(*h)->prev = new;
+		*h = new;
 		return (new);
 	}
-	while (current && count < idx)
+	while (current && count < idx - 1)
 	{
 		current = current->next;
 		count++;
 	}
 	if (!current)
+	{
+		free(new);
 		return (NULL);
-	new = add_dnodeint(h, n);
+	}
 	new->next =  current->next;
 	if (current->next)
 		current->next->prev = new;
