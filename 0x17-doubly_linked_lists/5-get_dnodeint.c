608e2f1dd8c4ef7ac1f0a7c470683f4aa7eebf99
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-17 13:23:34 +0100

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,24 @@
 #include "lists.h"
+
+/**
+ * dlistint_len - get length of dlist
+ * @h: head of dlist
+ * Return: number of node
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+	const dlistint_t *current = h;
+
+	while (current)
+	{
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
+
 /**
  * get_dnodeint_at_index - get node by index
  * @head: head of dlist
