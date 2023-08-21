7bd484101c2bc8a7aa2e2f2e3f1a1de3bf7d2b72
hamza-25 <me677499@gmail.com>
2023-08-21 15:07:23 +0000

    create math functions

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,35 @@
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+int sub(int a, int b)
+{
+        int diff;
+
+        diff = a - b;
+        return (diff);
+}
+int mul(int a, int b)
+{
+        int mult;
+
+        mult = a * b;
+        return (mult);
+}
+int div(int a, int b)
+{
+        int rslt;
+
+        rslt = a / b;
+        return (rslt);
+}
+int mod(int a, int b)
+{
+        int rest;
+
+        rest = a % b;
+        return (rest);
+}
