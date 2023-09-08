f9cc2d83a16c685ea64c7de53cea80926dd11e28
hamza-25 <me677499@gmail.com>
2023-09-08 18:28:06 +0000

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that delete hash table
+ * @ht: hash table
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *temp_node = NULL;
+
+	if (!ht)
+		return;
+	while (index < ht->size)
+	{
+		while (ht->array[index] != NULL)
+		{
+			temp_node = ht->array[index];
+			ht->array[index] = ht->array[index]->next;
+			free(temp_node->key);
+			free(temp_node->value);
+			free(temp_node);
+		}
+		index++;
+	}
+	free(ht);
+}
