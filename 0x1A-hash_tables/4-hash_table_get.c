4f196161ec86aa013abe34cb1c4baaf26ff4a063
hamza-25 <me677499@gmail.com>
2023-09-08 17:01:33 +0000

    update files

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,15 +9,16 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index, i;
+	unsigned long int index;
 
 	if (!ht || !key || *key == '\0')
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	while (ht->array[index])
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-			return (ht->array[i]->value);
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
 	}
 	return (NULL);
 }
