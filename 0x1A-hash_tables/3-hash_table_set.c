8089a9809e3d9a0e315fdf6e8b4ef04cf5a9aca1
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-08 16:42:41 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	if(!ht || !key || || !value)
+	if(!ht || !key || !value)
 		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
