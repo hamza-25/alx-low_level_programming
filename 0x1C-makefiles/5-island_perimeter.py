4960a821237106eb4a97a15c2a86dff0cd94229a
hamza-25 <me677499@gmail.com>
2023-10-25 17:16:33 +0000

    island task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+def perimeter1(grid):
+    pri = 0
+    for p_id, list in enumerate(grid):
+        index = 0
+        for elem in list:
+            if elem == 1:
+                pri += 4
+                if index + 1 < len(list) and list[index + 1] == 1:
+                    pri -= 1
+                if index - 1 >= 0 and list[index - 1] == 1:
+                    pri -= 1
+                if p_id + 1 < len(grid) and grid[p_id + 1][index] == 1:
+                    pri -= 1
+                if p_id - 1 >= 0 and grid[p_id - 1][index] == 1:
+                    pri -= 1
+            index += 1
+    return pri
